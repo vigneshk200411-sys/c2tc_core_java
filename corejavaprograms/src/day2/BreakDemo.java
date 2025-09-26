a0fd404e3c1f1ab8c9f42bf1bbed26b43ead0ec4
Asus <Asus@LAPTOP-UQOBRORU>
2025-09-26 23:26:44 +0530

    day2

---
+++ corejavaprograms/src/day2/BreakDemo.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class BreakDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 5; i < 10; i++) {
+			if (i == 5)
+				break;
+			System.out.println(i);
+		}
+
+	}
+
+}	
---
+++ corejavaprograms/src/day2/IfElseDemo.java
@@ -0,0 +1,18 @@
+package day2;
+
+public class IfElseDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int age=21;
+		
+		if(age>=18)
+			System.out.println(" You are eligible for Voting");	
+		else
+			System.out.println("Sorry! You need to wait more");
+
+		System.out.println("if else demonstration");
+
+	}
+
+}
