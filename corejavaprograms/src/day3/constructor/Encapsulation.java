43417c6a820954e2bb5e5f0990b4e082a00be493
Asus <Asus@LAPTOP-UQOBRORU>
2025-09-28 12:47:53 +0530

    day3

---
+++ corejavaprograms/src/day3/constructor/Encapsulation.java
@@ -0,0 +1,15 @@
+package day3.constructor;
+public class EncapsulationDemo {
+	public static void main(String[] args) {
+		
+		//object creation 
+		OopsConceptDemo obj = new OopsConceptDemo();
+		obj.setSerialNum(101);
+		obj.setName("Pooja");
+		obj.setAge(20);
+		System.out.println(obj);
+		
+	}
+
+}
+
